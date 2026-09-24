8aac397d7adc869cb93b19eed0c18f7a4afe6ee8
Keycy <keycyjmr30@gmail.com>
2026-09-23 21:06:03 -0500

    Corregidos errores de sintaxis

--- lab-git/src/Main.java
+++ lab-git/src/Main.java
@@ -14,5 +14,5 @@ void main() {
     System.out.println("hola");
     System.out.println("hola desde rama secundaria");
     int x = 500;
-    System.out.printl("hola Git")
+    System.out.println("hola Git");
 }
