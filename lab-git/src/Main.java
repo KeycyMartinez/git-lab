17bd7ebb13000d35626721b3e3c6eecf15e26741
Keycy <keycyjmr30@gmail.com>
2026-09-23 20:28:42 -0500

    Cambio x a 500 en master

--- lab-git/src/Main.java
+++ lab-git/src/Main.java
@@ -13,5 +13,5 @@ void main() {
     System.out.println("Modulo de Login");
     System.out.println("hola");
     System.out.println("hola desde rama secundaria");
-    int x = 50;
+    int x = 500;
 }
